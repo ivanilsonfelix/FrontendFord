2868a14e1e319eed3fcb88df0533621359145a45
Ivanilson Felix de Lima <127620119+ivanilsonfelix@users.noreply.github.com>
2023-11-18 17:59:10 -0300

    Add files via upload

---
+++ JavaScript/inicio/trabalhandoComDados/desfioArea..js
@@ -0,0 +1,11 @@
+
+// ATIVIDADE 1
+
+
+// CALCULAR A CIRCUFERENCIA DE UM CIRCULO
+
+
+const raio = 16;
+const area = Math.PI * Math.pow(raio,2)
+
+console.log(`Resultado ${raio} = ${area}`)
---
+++ JavaScript/inicio/trabalhandoComDados/script01.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// utilizando padrão camelCase
+// trabalhar com variaveis, console.log
+// variavel(let, var,const) - identificador - dados
+
+
+const carro = "carro"
+const fabricanteCarro = "Renault"
+const modeloCarro = "Sandero"
+const corCarro = "vermelho"
+const anoCarro = "2010"
+
+// Primeira forma: Concatenação
+console.log("carro: " + carro + 
+", fabricante: " + fabricanteCarro + " ,modelo do carro: " + modeloCarro + " ,cor do carro: " + corCarro + " ,ano do carro: " + anoCarro);
+
+// Segunda forma: Interpolação
+console.log (`meu carro: ${carro}, fabricante do carro: ${fabricanteCarro}, modelo do carro: ${modeloCarro}, cor do carro: ${corCarro}, ano do carro: ${anoCarro}`);
+
+// console.log(carro);
+// console.log(fabricanteCarro);
+// console.log(modeloCarro);
+// console.log(corCarro);
+// console.log(anoCarro);
+
