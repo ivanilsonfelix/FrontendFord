4307e23237c0396689e17e014db5b82dcad36f07
Ivanilson Félix <nil_si@yahoo.com.br>
2023-12-02 17:58:49 -0300

    add comando

--- JavaScript/inicio/trabalhandoComDados/Calculadora/script.js
+++ JavaScript/inicio/trabalhandoComDados/Calculadora/script.js
@@ -1,4 +1,6 @@
-function Calcular() {
+function Calcular(e) {
+    event.preventDefault()
+
     let n1 = parseInt(document.getElementById('num1').value)
     let n2 = parseInt(document.getElementById('num2').value)
     let operacao= document.getElementById("operacao").value
@@ -7,10 +9,10 @@ function Calcular() {
 
     // validadção par ao usuário não digitar palavras e apenas números
 
-    if (NaN(n1) && NaN(n2)){
-        alert("Somente números")
+    // if (NaN(n1) && NaN(n2)){
+        // alert("Somente números")
         
-    }
+    
     switch (operacao) {
         case 'Somar':
             resultado = n1 + n2
@@ -40,5 +42,5 @@ function Calcular() {
             alert("Opção inválida")
             break;
     }
-    
 }
+    
