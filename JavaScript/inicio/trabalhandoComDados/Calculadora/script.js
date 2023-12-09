a9054ddc38b73369ec62239d99b7580061782c0e
Your Name <you@example.com>
2023-12-09 11:19:01 -0300

    finalizando calculadora

--- JavaScript/inicio/trabalhandoComDados/Calculadora/script.js
+++ JavaScript/inicio/trabalhandoComDados/Calculadora/script.js
@@ -1,9 +1,12 @@
 function Calcular(e) {
-    event.preventDefault()
-
+    event.preventDefault() //impede de recarregar a página automaticamente
+    // valor capturado no input e guardando o valor na variavel n1
     let n1 = parseInt(document.getElementById('num1').value)
+    //valor capturado no input e guardando o valor na variavel n2
     let n2 = parseInt(document.getElementById('num2').value)
+    //operação escolhida pelo usuário (+, -, /, *)
     let operacao= document.getElementById("operacao").value
+    // variavel para guardar o resultado
     let resultado;
 
 
@@ -16,31 +19,49 @@ function Calcular(e) {
     switch (operacao) {
         case 'Somar':
             resultado = n1 + n2
-            alert(`O resultado da soma é: ${resultado}`)
+            document.getElementById(`resultado`).innerHTML = resultado
+    
             
             break;
 
         case 'Subtrair':
             resultado = n1 - n2
-            alert(`O resultado da subtração é: ${resultado}`)
+            document.getElementById(`resultado`).innerHTML = resultado
+    
             
             break;
 
         case 'Multiplicar':
             resultado = n1 * n2
-            alert(`O resultado da multiplicação é: ${resultado}`)
+            document.getElementById(`resultado`).innerHTML = resultado
+    
             
             break;
 
         case 'Dividir':
-            resultado = n1 / n2
-            alert(`O resultado da divisão é: ${resultado}`)
+                       
+            if (n1 == 0 || n2 == 0) {
+            //resultado = n1 / n2
+            const resp = ("Não é possivel dividir por 0")
+            document.getElementById("resultado").innerHTML =  resp
+            } else {
+                resultado = n1 / n2
+                document.getElementById(`resultado`).innerHTML = resultado
+    
+                
+            }
+                
+            
             
             break;
     
         default:
             alert("Opção inválida")
             break;
+
+            
     }
+    
+    
 }
     
